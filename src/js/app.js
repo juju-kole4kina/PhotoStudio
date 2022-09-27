7f8407540de32e873c145d1bc0e1778f057fdfaf
Juliya Koltsova <juliyaasking009@gmail.com>
2022-09-27 21:37:46 +0200

    burger icon add

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,12 @@ window['FLS'] = true;
 //import "../scss/style.scss";
 
 import * as flsFunctions from "./modules/functions.js";
+import './modules/dinamic_adapt.js';
 
 
 flsFunctions.isWebp();
-
+flsFunctions.isMob();
+flsFunctions.burger();
 
 
 
